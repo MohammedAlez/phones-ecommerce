83f9b539ea7a135fd20801519ddae8104149be00
MohammedAlez <125201940+MohammedAlez@users.noreply.github.com>
2025-01-24 11:41:01 +0100

    update footer image

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -61,8 +61,8 @@ export default function Footer(){
                     {/* forth block */}
                     <div className="flex-1 text-white">
                     <div className="">
-                        <div className="mx-auto md:ml-0 w-20 h-20 md:w-28 md:min-h-28 relative md:mx-auto rounded-full overflow-hidden">
-                            <Image src='/logo.jpg' fill alt='' className="" />
+                        <div className="mx-auto md:ml-0 w-20 h-20 md:w-28 md:min-h-28 relative md:mx-auto rounded-full overflow-hidden bg-gray-400">
+                            <Image src='/logo.jpg' fill alt='' className="bg-gray-400" />
                         </div>
                     </div>
                         <p className="py-6 md:text-start max-w-full text-center text-sm">
